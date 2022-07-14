ea1350ba955e4214286d909ce62b2204eef9dfbe
xxf <2103625@qq.com>
2022-07-14 20:50:26 +0800

    update

--- samples/BrunSample/Program.cs
+++ samples/BrunSample/Program.cs
@@ -15,7 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-//Ç°¶Ë¿ª·¢»·¾³ÅäÖÃ
+//å‰ç«¯å¼€å‘ç¯å¢ƒé…ç½®
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(config =>
@@ -30,34 +30,34 @@ builder.Services.AddCors(options =>
 
 builder.Services.AddBrunService(options =>
 {
-    //TODO ³õÊ¼»¯¼ÓÔØÆäËü³ÌĞò¼¯
+    //TODO åˆå§‹åŒ–åŠ è½½å…¶å®ƒç¨‹åºé›†
     Brun.Commons.BrunTool.LoadFile("BrunTestHelper.dll");
     //options.UseRedis("192.168.1.4");
     options.UseInMemory();
     //options.UseStore(builder.Configuration.GetConnectionString("brun"), DbType.PostgreSQL);
-    //³ÌĞòÆô¶¯Ê±´´½¨WorkerºÍBackRun
+    //ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ê±ï¿½ï¿½ï¿½ï¿½Workerï¿½ï¿½BackRun
     options.InitWorkers = workers =>
     {
-        //Ìí¼ÓWorker
+        //ï¿½ï¿½ï¿½ï¿½Worker
        // IOnceWorker once = workers.AddOnceWorker(new WorkerConfig("once_1", ""));
         IQueueWorker queue = workers.AddQueueWorker(new WorkerConfig("queue_1", ""));
         ITimeWorker time = workers.AddTimeWorker(new WorkerConfig());
         IPlanWorker plan = workers.AddPlanWorker(new WorkerConfig());
-        //ÅäÖÃBackRun
+        //ï¿½ï¿½ï¿½ï¿½BackRun
         //once.AddBrun<BrunTestHelper.BackRuns.AwaitErrorBackRun>(new OnceBackRunOption("onceB_1", "onceB_Name"));
         queue.AddBrun<BrunTestHelper.QueueBackRuns.LogQueueBackRun>(new QueueBackRunOption());
-        time.AddBrun<BrunTestHelper.LogTimeBackRun>(new TimeBackRunOption(TimeSpan.FromSeconds(5)));//5ÃëÖ´ĞĞÒ»´Î
-        time.AddBrun<BrunTestHelper.TimeErrorBackRun>(new TimeBackRunOption(TimeSpan.FromSeconds(5)));//5ÃëÖ´ĞĞÒ»´Î 
-        time.AddBrun<BrunTestHelper.LogTimeLongBackRun>(new TimeBackRunOption(TimeSpan.FromSeconds(5)));//5ÃëÖ´ĞĞÒ»´Î
-        plan.AddBrun<BrunTestHelper.LogPlanBackRun>(new PlanBackRunOption(new PlanTime("0 * * * *")));//Ã¿·ÖÖÓÖ´ĞĞ
-        plan.AddBrun<BrunTestHelper.LogPlanBackRun>(new PlanBackRunOption(PlanTime.Create("0/5 * * * *")));//5ÃëÖ´ĞĞÒ»´Î
+        time.AddBrun<BrunTestHelper.LogTimeBackRun>(new TimeBackRunOption(TimeSpan.FromSeconds(5)));//5ï¿½ï¿½Ö´ï¿½ï¿½Ò»ï¿½ï¿½
+        time.AddBrun<BrunTestHelper.TimeErrorBackRun>(new TimeBackRunOption(TimeSpan.FromSeconds(5)));//5ï¿½ï¿½Ö´ï¿½ï¿½Ò»ï¿½ï¿½ 
+        time.AddBrun<BrunTestHelper.LogTimeLongBackRun>(new TimeBackRunOption(TimeSpan.FromSeconds(5)));//5ï¿½ï¿½Ö´ï¿½ï¿½Ò»ï¿½ï¿½
+        plan.AddBrun<BrunTestHelper.LogPlanBackRun>(new PlanBackRunOption(new PlanTime("0 * * * *")));//Ã¿ï¿½ï¿½ï¿½ï¿½Ö´ï¿½ï¿½
+        plan.AddBrun<BrunTestHelper.LogPlanBackRun>(new PlanBackRunOption(PlanTime.Create("0/5 * * * *")));//5ï¿½ï¿½Ö´ï¿½ï¿½Ò»ï¿½ï¿½
     };
-}).AddBrunUI(authoptions =>//Æô¶¯UI×é¼ş
+}).AddBrunUI(authoptions =>//å¯åŠ¨UIç»„ä»¶
 {
     authoptions.AuthType = AuthType.SimpleToken;
     authoptions.UserName = "admin";
     authoptions.Password = "admin";
-    //»òÕßappsettingÖĞÅäÖÃÑ¡Ïî£¬key£ºBrunAuthenticationScheme£¬ÎÄµµ£ºhttps://docs.microsoft.com/zh-cn/aspnet/core/fundamentals/configuration/options?view=aspnetcore-6.0#use-ioptionssnapshot-to-read-updated-data
+    //æˆ–è€…appsettingä¸­é…ç½®é€‰é¡¹ï¼Œkeyï¼šBrunAuthenticationSchemeï¼Œæ–‡æ¡£ï¼šhttps://docs.microsoft.com/zh-cn/aspnet/core/fundamentals/configuration/options?view=aspnetcore-6.0#use-ioptionssnapshot-to-read-updated-data
 });
 
 var app = builder.Build();
@@ -78,7 +78,7 @@ app.UseBrunUI();
 
 app.UseRouting();
 
-//Ç°¶Ë¿ª·¢»·¾³ÅäÖÃ
+//å‰ç«¯å¼€å‘ç¯å¢ƒé…ç½®
 app.UseCors();
 
 app.UseAuthentication();
